b55fda82408f27ea139ff7deb01841d377a9834f
Anas Boulif <a.boulif@esisa.ac.ma>
2026-03-08 16:40:29 +0000

    feat: add sitemap and robots.txt for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
   site: 'https://portfolio-anas-boulif.vercel.app',
+  integrations: [sitemap()],
 });
